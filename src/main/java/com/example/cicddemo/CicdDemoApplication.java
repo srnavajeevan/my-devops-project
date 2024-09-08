138c9dbe4aa4b8b3ad8ac29d351dc4e7a04a912d
srnavajeevan <srnavajeevan@gmail.com>
2024-09-08 22:11:28 +0530

    Add files via upload

---
+++ src/main/java/com/example/cicddemo/CicdDemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("sample cicd project..");
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+
+}
+
+
+
